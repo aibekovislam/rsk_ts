9ddf18dd01530871ebaac1def6aba4bc12416f05
omurbekov7756 <121441028+omurbekov7756@users.noreply.github.com>
2023-07-01 15:12:00 +0600

    Delete src/utils directory

--- src/utils/const.ts
+++
@@ -1,9 +0,0 @@
-export const BASE_URL = "http://35.184.55.194";
-
-export const ACTIONS = {
-  products: "products",
-  categories: "categories",
-  oneProduct: "oneProduct",
-  pageTotalCount: "pageTotalCount",
-  user: "user",
-} as const;
--- src/utils/consts.js
+++
@@ -1,6 +0,0 @@
-export const BASE_URL = "http://35.184.55.194";
-
-export const ACTIONS = {
-    queues: "queues",
-    rejectedQueue: "rejectedQueue"
-}
